9c8188e4527bafca5555554a9c59166de51138d1
KeithNull <s101315@gmail.com>
2023-06-01 02:31:03 +0000

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+  assetPrefix: isProd ? '/portfolio/' : '',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
