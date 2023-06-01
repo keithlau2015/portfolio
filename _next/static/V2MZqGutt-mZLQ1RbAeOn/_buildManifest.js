8320c26e00add467c1995ae1709b0bfd1f099548
keithlau2015 <keithlau2015@users.noreply.github.com>
2023-06-01 07:45:17 +0000

    deploy: cc2315877046a37fed5ff127437cc8b6b5e332c2

---
+++ _next/static/V2MZqGutt-mZLQ1RbAeOn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fbae4c59b6931427.css","static/chunks/pages/index-bd3b03feb3290993.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
