0d474c494cff40f1d0f83d2d7a86ad05a0d9b335
KeithNull <s101315@gmail.com>
2023-06-05 07:39:31 +0000

    update page

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`zflex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`zflex min-h-screen flex-col items-center ${inter.className}`}
     >
       <Head>
         <title>Keith🎮Game Developer</title>
@@ -16,7 +16,39 @@ export default function Home() {
         <link rel="icon" type="image/png" sizes="16x16" href="https://keithlau2015.github.io/portfolio/icons/favicon-16x16.png"/>
       </Head>      
       
-      
+      <div class="relative overflow-hidden bg-cover bg-no-repeat" style=
+      {{
+        backgroundPosition: '100%',
+        backgroundImage: 'https://mdbcdn.b-cdn.net/img/new/slides/146.webp',
+        height: '700px'
+      }}>
+        <div
+          class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.75)] bg-fixed">
+          <div class="flex h-full items-center justify-center">
+            <div class="px-6 text-center text-white md:px-12">
+              <h1 class="mt-2 mb-16 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl">
+                KEITH NULL
+                <br/>
+                <div class="horizontal"><div class="hr"></div><img src="/static/media/diamond.f7a7deb7.png" class="diamond" alt=""/><div class="hr"></div></div>
+                <br/> GAME DEVELOPER
+              </h1>
+              <button type="button"
+                class="rounded border-2 border-neutral-50 px-[46px] pt-[14px] pb-[12px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-100 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200"
+                data-te-ripple-init data-te-ripple-color="light">
+                Get started
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="text-center">
+        <img
+          src="https://keithlau2015.github.io/portfolio/icons/android-chrome-512x512.png"
+          class="mx-auto mb-4 w-32 rounded-lg"
+          alt="Avatar" />
+        <h5 class="mb-2 text-xl font-medium leading-tight">Keith</h5>
+        <p class="text-neutral-500 dark:text-neutral-400">Game Developer</p>
+      </div>
 
     </main>
   )
