3893bcdebae70890124bd423553d2b45b7a1ab20
KeithNull <s101315@gmail.com>
2023-06-06 11:35:43 +0000

    Update Icon

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,22 @@ export default function Home() {
         <link rel="apple-touch-icon" sizes="180x180" href="https://keithlau2015.github.io/portfolio/icons/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="https://keithlau2015.github.io/portfolio/icons/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="https://keithlau2015.github.io/portfolio/icons/favicon-16x16.png"/>
+        <link rel="apple-touch-icon" sizes="57x57" href="https://keithlau2015.github.io/portfolio/icons/apple-icon-57x57.png"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="https://keithlau2015.github.io/portfolio/icons/apple-icon-60x60.png"/>
+        <link rel="apple-touch-icon" sizes="72x72" href="https://keithlau2015.github.io/portfolio/icons/apple-icon-72x72.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="https://keithlau2015.github.io/portfolio/icons/apple-icon-76x76.png"/>
+        <link rel="apple-touch-icon" sizes="114x114" href="https://keithlau2015.github.io/portfolio/icons/apple-icon-114x114.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="https://keithlau2015.github.io/portfolio/icons/apple-icon-120x120.png"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="https://keithlau2015.github.io/portfolio/icons/apple-icon-144x144.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="https://keithlau2015.github.io/portfolio/icons/apple-icon-152x152.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="https://keithlau2015.github.io/portfolio/icons/apple-icon-180x180.png"/>
+        <link rel="icon" type="image/png" sizes="192x192"  href="https://keithlau2015.github.io/portfolio/icons/android-icon-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="https://keithlau2015.github.io/portfolio/icons/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="https://keithlau2015.github.io/portfolio/icons/favicon-96x96.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="https://keithlau2015.github.io/portfolio/icons/favicon-16x16.png"/>
+        <link rel="manifest" href="https://keithlau2015.github.io/portfolio/icons/manifest.json"/>
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="msapplication-TileImage" content="https://keithlau2015.github.io/portfolio/icons/ms-icon-144x144.png"/>
       </Head>      
       <section id="Landing">
       <div class="relative overflow-hidden bg-cover bg-no-repeat" style=
