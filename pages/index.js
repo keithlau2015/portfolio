b8eb1bd4276abc265b00fc9af47d9d05bbc97770
KeithNull <s101315@gmail.com>
2023-06-06 13:49:37 +0000

    update icon

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,13 @@ export default function Home() {
       <Head>
         <title>Keith🎮Game Developer</title>        
         <link rel="icon" type="image/png" href="https://keithlau2015.github.io/portfolio/icons/MyIcon_p_NBG.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="https://keithlau2015.github.io/portfolio/icons/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="https://keithlau2015.github.io/portfolio/icons/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="https://keithlau2015.github.io/portfolio/icons/favicon-16x16.png"/>
+        <link rel="manifest" href="https://keithlau2015.github.io/portfolio/iconssite.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>      
       <section id="Landing">
       <div class="relative overflow-hidden bg-cover bg-no-repeat" style=
