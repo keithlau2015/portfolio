3e56a57993e2b059ffa7a391a42a5c86afa1859f
KeithNull <s101315@gmail.com>
2023-06-07 09:30:38 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -29,10 +29,7 @@ export default function Home() {
         <link rel="apple-touch-icon" sizes="180x180" href="https://keithlau2015.github.io/portfolio/icons/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="https://keithlau2015.github.io/portfolio/icons/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="https://keithlau2015.github.io/portfolio/icons/favicon-16x16.png"/>
-        <link rel="manifest" href="https://keithlau2015.github.io/portfolio/iconssite.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
-        <meta name="msapplication-TileColor" content="#da532c"/>
-        <meta name="theme-color" content="#ffffff"></meta>
+        <link rel="manifest" href="https://keithlau2015.github.io/portfolio/site.webmanifest"/>
       </Head>      
       <section id="Landing">
       <div class="relative overflow-hidden bg-cover bg-no-repeat" style=
