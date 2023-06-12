693659ef7936f6effbd9afec87ad58c66ca6aab9
KeithNull <s101315@gmail.com>
2023-06-12 18:01:30 +0800

    Update projectCard.js

--- pages/project/projectCard.js
+++ pages/project/projectCard.js
@@ -1,5 +1,5 @@
 import { ProjectDetailsPopover } from "./projectDetails";
-export function ProjectCard({title, description, bg, owner, type, subType, duty}) {
+export default function ProjectCard({title, description, bg, owner, type, subType, duty}) {
     return (<div className={`
         group grid grid-cols-1 gap-1 content-end mx-auto rounded-lg hover:shadow-md hover:shadow-black/80 w-[250px] h-[250px] transition duration-300 ease-in-out hover:scale-110 bg-center bg-cover 
         bg-[linear-gradient(to_bottom,rgba(0,0,0,0.1),rgba(0,0,0,0.9)),${bg}] 
