881802f69c0ef74534f2f899dd7075c08fb65dde
KeithNull <s101315@gmail.com>
2023-06-12 09:48:06 +0000

    Update project struct

---
+++ pages/milestone/milestoneSection.js
@@ -0,0 +1,25 @@
+import { OneMilestone } from "./oneMilestone";
+
+export function MilestoneSection() {
+    const lifeTimeEvents = [
+        {eventName:"Karin Group", range:"12/2022 - 12/2023", details:"Analyst Programmer"},
+        {eventName:"Dream Global Entertainment Limited", range:"07/2020 - 09/2022", details:"Cocos Game Development Engineer"},
+        {eventName:"Gameone Group Limited", range:"06/2020 - 07/2022", details:"Senior Programmer"},
+        {eventName:"Graduate from Tamkang University", range:"09/2016 - 06/2020", details:"Innovative Information and Technology Department"}
+    ];
+
+    return (<ol className="relative ml-20 border-l border-gray-200 dark:border-gray-700">     
+        {
+            lifeTimeEvents.map((item,index)=>{
+                return <li class="mb-10 ml-10">
+                <OneMilestone 
+                    eventName={item.eventName}
+                    range={item.range}
+                    details={item.details}
+                    index={index}
+                />
+                </li>
+            })
+        }
+  </ol>);
+}
---
+++ pages/milestone/oneMilestone.js
@@ -0,0 +1,15 @@
+export function OneMilestone({eventName, range, details, index}) {
+    return (<div>
+    <span className="absolute flex items-center justify-center w-6 h-6 bg-amber-300 rounded-full -left-3 ring-8 ring-white">
+        <svg aria-hidden="true" className="w-3 h-3 text-amber-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+    </span>
+    <h3 className="flex items-center mb-1 text-lg font-semibold text-white">{eventName?eventName:"Name"}
+    {
+        index==0 && 
+        <span className="bg-amber-300 text-amber-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded ml-3">Latest</span>
+    }
+    </h3>
+    <time className="block mb-2 text-sm font-normal leading-none text-gray-400">{range?range:"00/0000 - 00/0000"}</time>
+    <p className="mb-4 text-base font-normal text-gray-500">{details?details:""}</p>
+  </div>);
+}
