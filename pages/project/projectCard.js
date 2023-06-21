3b10ba90b5015efeed1debffd44a6f0633b274cf
KeithNull <s101315@gmail.com>
2023-06-21 09:28:28 +0800

    Update projectCard.js

--- pages/project/projectCard.js
+++ pages/project/projectCard.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 import { ProjectDetailsPopover } from "./projectDetails";
 
 export function ProjectCard({title, description, bg, owner, type, subType, duty, tech, index, url}) {
-    return (
-            url!=""?
+    return ({
+            url!="" &&
             <Link href={url} passHref={true}>
                 <div class={`
                     group grid grid-cols-1 gap-1 content-end mx-auto rounded-lg hover:shadow-md hover:shadow-black/80 w-[250px] h-[250px] transition duration-300 ease-in-out hover:scale-110 bg-center bg-cover 
@@ -14,7 +14,9 @@ export function ProjectCard({title, description, bg, owner, type, subType, duty,
                     <div class="text-xs text-white pb-5">{description ? description : 'Description'}</div>
                 </div>
             </Link>
-            :
+}
+{
+            url=="" &&
             <div class={`
                 group grid grid-cols-1 gap-1 content-end mx-auto rounded-lg hover:shadow-md hover:shadow-black/80 w-[250px] h-[250px] transition duration-300 ease-in-out hover:scale-110 bg-center bg-cover 
                 ${bg}
@@ -24,5 +26,6 @@ export function ProjectCard({title, description, bg, owner, type, subType, duty,
                 <div class="text-xs text-white pb-5">{description ? description : 'Description'}</div>
             </div>
     );
+                }
 }
  export default ProjectCard;
