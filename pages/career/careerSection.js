ae7eb88ac1831e83f78a1ae2fcd0a45dc366bb49
Keith Null <s101315@gmail.com>
2024-01-03 10:35:47 +0800

    Update Career

--- pages/career/careerSection.js
+++ pages/career/careerSection.js
@@ -1,7 +1,8 @@
 import { OneCareer } from "./oneCareer";
 
 export const workingExpEvents = [
-    {eventName:"Karin Group", range:"12/2022 - 12/2023", details:"Analyst Programmer"},
+    {eventName:"Nexify Limited", range:"1/2024 - Current", details:"Analyst Programmer"},
+    {eventName:"Kepro Solution Limited", range:"12/2022 - 12/2023", details:"Analyst Programmer"},
     {eventName:"Dream Global Entertainment Limited", range:"07/2020 - 09/2022", details:"Cocos Game Development Engineer"},
     {eventName:"Gameone Group Limited", range:"06/2020 - 07/2022", details:"Senior Programmer"},
     {eventName:"Graduate from Tamkang University", range:"09/2016 - 06/2020", details:"Innovative Information and Technology Department"}
